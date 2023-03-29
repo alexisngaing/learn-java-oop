2245e340f1b4cce37de2579479a2bb69d2a34565
Alexis Ngaing <ksankirara92@gmail.com>
2023-03-29 16:34:37 +0700

    update

--- 04 - Polimorfism/UGD5_B_11_Latihan/src/ugd5_b_11_latihan/IBiaya.java
+++
@@ -1,9 +0,0 @@
-package ugd5_b_11_latihan;
-
-/**
- *
- * @author ASUS
- */
-public interface IBiaya {
-    public double hitungBiaya();
-}
--- 04 - Polimorfism/UGD5_B_11_Latihan/src/ugd5_b_11_latihan/IDurasi.java
+++
@@ -1,9 +0,0 @@
-package ugd5_b_11_latihan;
-
-/**
- *
- * @author ASUS
- */
-public interface IDurasi {
-    public double hitungDurasi();
-}
