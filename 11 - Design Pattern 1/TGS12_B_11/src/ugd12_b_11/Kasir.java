b42351d029c7e91a491dad8b6b42f6735a7642a3
Alexis Ngaing <ksankirara92@gmail.com>
2023-05-29 20:46:39 +0700

    tugas

---
+++ 11 - Design Pattern 1/TGS12_B_11/src/ugd12_b_11/Kasir.java
@@ -0,0 +1,23 @@
+package ugd12_b_11;
+
+public class Kasir extends Karyawan {
+    private final int jumlahLembur;
+    
+    public Kasir(String nama, double tunjangan, Toko toko, int jumlahLembur) {
+        super(nama, tunjangan, toko);
+        this.jumlahLembur = jumlahLembur;
+    }
+    
+    public void ShowData() {
+        System.out.println("[Kasir] " + nama + " - " + "Total Tunjangan : " + tunjangan);
+    }
+    public void Update(double jumlah) {
+        double bonusKasir = jumlah + (jumlahLembur * 10000);
+        System.out.println("\t--- Tunjangan " + nama + " bertambah : Rp " + bonusKasir);
+        this.tunjangan += bonusKasir;
+    }
+    
+    public double GetTunjangan() {
+        return tunjangan;
+    }
+}
---
+++ 11 - Design Pattern 1/TGS12_B_11/src/ugd12_b_11/Toko.java
@@ -0,0 +1,58 @@
+package ugd12_b_11;
+
+import java.util.ArrayList;
+
+public class Toko {
+    private final String namaToko;
+    private final String alamat;
+    private Manager penanggungJawab;
+    private final ArrayList<Karyawan> karyawan;
+    private double danaToko = 20000000;
+    
+    public Toko(String namaToko, String alamat) {
+        this.namaToko = namaToko;
+        this.alamat = alamat;
+        this.karyawan = new ArrayList<Karyawan>();
+    }
+    
+    public void TampilDaftarKaryawan() {
+        System.out.println("Nama Toko   : " + this.namaToko);
+        System.out.println("Alamat      : " + this.alamat);
+        System.out.println("Dana Toko   : " + this.danaToko);
+        penanggungJawab.ShowData();
+    }
+    
+    public void RegisterObserver(Karyawan k) {
+        karyawan.add(k);
+    }
+    
+    public void NotifyObserver(double jumlah) {
+        for (Karyawan k : karyawan) {
+            k.Update(jumlah);
+        }
+    }
+    
+    public void PenjualanBarang(double harga, int jmlBarang) {
+        double bonus = (harga * jmlBarang) / 20;
+        double keuntungan = harga * jmlBarang;
+
+        System.out.println(Karyawan.space + "Tampilkan Observernya saat terjadi penjualan dengan jumlah barang " + jmlBarang + " dan harga " + harga);
+        Karyawan.space.append("   ");
+        System.out.println("\t\tToko mendapatkan dana sebesar : " + keuntungan);
+        this.danaToko = this.danaToko + keuntungan;
+        NotifyObserver(bonus);
+        Karyawan.space.setLength(Karyawan.space.length() - 3);
+    }
+    
+    public void SetPenanggungJawab(Manager manager) {
+//        if (!penanggungJawab.getJenis().equalsIgnoreCase("Utama")) {
+//            System.err.println("Jabatan Manager kurang tinggi!");
+//        } else {
+            this.penanggungJawab = manager;
+//        }
+    }
+    
+    public void ShowTotalTunjangan() {
+        System.out.println("\nTotal Tunjangan : " + penanggungJawab.GetTunjangan());
+    }
+}
